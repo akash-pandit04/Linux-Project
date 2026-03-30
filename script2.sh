30cdcfaf9815b43f7f8d0df5cc408ae7655e52f8
Akash Kumar Pandit <110347257+akash-pandit04@users.noreply.github.com>
2026-03-30 22:47:01 +0530

    Create script2.sh

---
+++ script2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Script 2: Package Check and Details
+
+# Author: Akash Kumar Pandit | Course: Open Source Software
+
+
+# --- Variable --- PACKAGE="python3"
+
+echo "================================"
+echo " Checking package: $PACKAGE"
+echo "================================"
+
+
+# Check if package is installed
+if dpkg -l | grep -q "$PACKAGE"; then echo "$PACKAGE is installed."
+ 
+echo ""
+echo "Package Details:" dpkg -l | grep "$PACKAGE" echo ""
+echo "Description:"
+apt show "$PACKAGE" | grep Description else
+echo "$PACKAGE is NOT installed." 
